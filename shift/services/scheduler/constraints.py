8fd55526c23365a8f97b1adf08c36ecac2d790ef
Huub Hoofs <huubhoofs@gmail.com>
2023-11-21 22:15:25 +0100

    add contraints

--- shift/services/scheduler/constraints.py
+++ shift/services/scheduler/constraints.py
@@ -4,7 +4,7 @@ from typing import Iterable
 from shift.services.scheduler.models import create_key
 
 
-def workers_per_shift(model, vars, workers, n, *args: Iterable[tuple]):
+def workers_per_shift(model, vars, workers, n=1, *args: Iterable[tuple]):
     if n != 1:
         raise ValueError(
             f"Planning works only with 1 required worker for each shift. "
@@ -14,3 +14,15 @@ def workers_per_shift(model, vars, workers, n, *args: Iterable[tuple]):
     
     for key in product(*args):
         model.AddExactlyOne(vars[create_key(worker, *key)] for worker in workers)
+
+
+def shifts_per_day(model, vars, shifts, n=1, *args: Iterable[tuple]):
+    if n != 1:
+        raise ValueError(
+            f"Planning works only with 1 required worker for each shift. "
+            f"It is therefore not possible to plan with the specified "
+            f"{n} workers within each shift"
+        )
+    
+    for key in product(*args):
+        model.AddAtMostOne(vars[create_key(shift, *key)] for shift in shifts)
--- shift/services/scheduler/models.py
+++ shift/services/scheduler/models.py
@@ -32,6 +32,9 @@ class ConstraintModel:
         constraints.workers_per_shift(
             self._model, self._vars, self._workers, 1, self._shifts, self._reps
         )
+        constraints.shifts_per_day(
+            self._model, self._vars, self._shifts, 1, self._reps, self._workers
+        )
 
     @staticmethod
     def _create_vars(
@@ -42,9 +45,10 @@ class ConstraintModel:
 
 
 def create_key(*vars: Iterable[DomainModel]):
-    key = []
-    for var in vars:
-        key.append((var.id, var.domain))
+    sorted_keys = sorted(vars, key=lambda var: var.domain)
+    generated_keys = [f'{var.domain}-{var.id}' for var in sorted_keys]
+    cp_key = '|'.join(generated_keys)
+    return cp_key
 
 
 if __name__ == "__main__":
@@ -53,4 +57,4 @@ if __name__ == "__main__":
     cm = ConstraintModel(workers, shifts, 1)
     cm.add_vars_to_model()
     cm.add_constraints_to_model()
-    cm
+    solv
