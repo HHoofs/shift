9d97f6a081c0c2b87e6e86bbfe7099b9aa40c425
Huub Hoofs <huubhoofs@gmail.com>
2024-01-17 13:59:23 +0100

    yes

--- shift/domain/planning.py
+++ shift/domain/planning.py
@@ -1,10 +1,14 @@
-from calendar import Day
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from datetime import date
 from typing import Iterable, Iterator
 
+from ortools.sat.python import cp_model
+from ortools.sat.python.cp_model import CpModel, LinearExprT
+
 from shift.domain.base import Model
-from shift.domain.shift import Period, Shift, Slot, shift_range
+from shift.domain.shift import Day, Period, Shift, Slot, shift_range
 
 
 @dataclass
@@ -15,6 +19,7 @@ class Planning(Model):
     shift_duration: int
     employees_per_shift: int
     employee_ids: list[int] = field(default_factory=list)
+    constraints: list[ModelConstraint] = field(default_factory=list)
 
     def get_slots(self) -> Iterable[Slot]:
         first_shift = Shift(min(self.periods), Day(self.first_day))
@@ -29,3 +34,51 @@ class Planning(Model):
                 duration=self.shift_duration,
                 n_employees=self.employees_per_shift,
             )
+
+
+@dataclass
+class ModelConstraint(Model):
+    employee_ids: list[int] = field(default_factory=list)
+
+    def add_constraint(
+        self,
+        slots: Iterable[Slot],
+        model: cp_model.CpModel,
+        employee_slots: dict[str, cp_model.LinearExprT],
+    ) -> None:
+        raise NotImplementedError
+
+
+@dataclass
+class WorkersPerShift(ModelConstraint):
+    def add_constraint(
+        self,
+        slots: Iterable[Slot],
+        model: CpModel,
+        employee_slots: dict[str, LinearExprT],
+    ) -> None:
+        for slot in slots:
+            _sum = sum(
+                employee_slots[...] for employee_id in self.employee_ids
+            )
+            model.Add(_sum == slot.n_employees)
+
+
+# def workers_per_shift(
+#     model: cp_model.CpModel,
+#     vars: Mapping[str, cp_model.LinearExprT],
+#     workers,
+#     n=1,
+#     *args: Iterable[DomainModels],
+# ):
+#     if n != 1:
+#         raise ValueError(
+#             f"Planning works only with 1 required worker for each shift. "
+#             f"It is therefore not possible to plan with the specified "
+#             f"{n} workers within each shift"
+#         )
+
+#     for key in product(*args):
+#         model.AddExactlyOne(
+#             vars[create_key(worker, *key)] for worker in workers
+#         )
