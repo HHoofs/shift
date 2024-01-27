473c000c47ceac2e5eaf7ef59fdde4e458d14ff4
Huub Hoofs <huubhoofs@gmail.com>
2024-01-27 22:59:43 +0100

    Check

--- shift/domain/solver/__init__.py
+++ shift/domain/solver/__init__.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, field
 from itertools import product
-from typing import Iterable, Optional
+from typing import Iterable, Optional, Sequence
 
 from ortools.sat.python import cp_model  # type: ignore
 
 from shift.domain.base import Model
 from shift.domain.model import EmployeeSlot, get_key
-from shift.domain.shift import Shift
+from shift.domain.planning.constraints import PlanningConstraint
+from shift.domain.planning.distributions import PlanningDistribution
+from shift.domain.shift import Shift, Slot
 from shift.domain.solver.optimizers import PlanningOptimization
 
 
@@ -18,6 +20,8 @@ class Solver(Model):
     employee_slots: dict[EmployeeSlot, cp_model.IntVar] = field(
         default_factory=dict
     )
+    added_constraints: list[int] = field(default_factory=list)
+    added_distributions: list[int] = field(default_factory=list)
 
     def add_slots(
         self, employee_ids: Iterable[int], shifts: Iterable[Shift]
@@ -27,4 +31,26 @@ class Solver(Model):
                 get_key(employee_id, shift)
             ] = self.model.NewBoolVar(
                 f"Slot <Employee: {employee_id}; Shift: {shift}"
+            )
+
+    def add_constraints(
+        self, constraints: Iterable[PlanningConstraint], slots: Sequence[Slot]
+    ) -> None:
+        for constraint in constraints:
+            constraint.add_constraint(
+                model=self.model,
+                employee_slots=self.employee_slots,
+                slots=slots,
+            )
+
+    def add_distributions(
+        self,
+        distributions: Iterable[PlanningDistribution],
+        slots: Sequence[Slot],
+    ) -> None:
+        for distribution in distributions:
+            distribution.add_distribution(
+                model=self.model,
+                employee_slots=self.employee_slots,
+                slots=slots,
             )
